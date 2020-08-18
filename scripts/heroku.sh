33a95da93e451b53192cf4fc67f1eb5a45c0ed04
Jai Gupta <jai.jgec@gmail.com>
2020-08-18 14:35:52 +0530

    heroku.sh script for deployment

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -4,6 +4,7 @@ mkdir -p test-reports
 rm -f test-reports/heroku_deployment.log
 
 echo "### Deploying the Heroku app from DockerHub ###" >> test-reports/heroku_deployment.log
+heroku -v >> test-reports/heroku_deployment.log
 docker -v >> test-reports/heroku_deployment.log
 
 echo "Login to DockerHub" >> test-reports/heroku_deployment.log
@@ -14,10 +15,6 @@ docker pull jai6016/carthage_docker_app >> test-reports/heroku_deployment.log
 
 echo "Re-tag the Docker Image with the heroku_docker_app tag" >> test-reports/heroku_deployment.log
 docker tag jai6016/carthage_docker_app heroku_docker_app >> test-reports/dockerize.log
-
-echo "Set the HEROKU_APP_KEY for access" >> test-reports/heroku_deployment.log
-heroku -v >> test-reports/heroku_deployment.log
-export HEROKU_API_KEY=c4a9906d-2b97-45b0-80eb-d11f8a9b15b6
 
 echo "Destroy the Heroku app if exists" >> test-reports/heroku_deployment.log
 heroku apps:destroy -a carthage-heroku-app -c carthage-heroku-app >> test-reports/heroku_deployment.log
